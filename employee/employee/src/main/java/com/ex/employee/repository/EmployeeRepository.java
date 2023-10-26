9c6d5827080d42a78ab7b4ab886b06e8b03cff2f
Shrinivas1993 <148951420+Shrinivas1993@users.noreply.github.com>
2023-10-26 15:17:00 +0530

    Delete employee/employee directory

--- employee/employee/src/main/java/com/ex/employee/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ex.employee.repository;
-
-import com.ex.employee.model.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,Long> {
-}
